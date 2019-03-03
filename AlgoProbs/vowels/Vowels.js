047ebccd3737c3b4b5b64e444f3eff9a21a2ddf1
Christine Gomez <christine.gomez1226@gmail.com>
2019-03-02 22:52:02 -0500

    solution for vowels

---
+++ AlgoProbs/vowels/Vowels.js
@@ -0,0 +1,21 @@
+// --- Directions
+// Write a function that returns the number of vowels
+// used in a string.  Vowels are the characters 'a', 'e'
+// 'i', 'o', and 'u'.
+// --- Examples
+//   vowels('Hi There!') --> 3
+//   vowels('Why do you ask?') --> 4
+//   vowels('Why?') --> 0
+
+function vowels(str) {
+  let vowel = 'aeiou'
+  let total = 0
+  for(let el of str.toLowerCase()){
+    if(vowel.includes(el)){
+      ++total
+    }
+  }
+  return total
+}
+
+module.exports = vowels;
