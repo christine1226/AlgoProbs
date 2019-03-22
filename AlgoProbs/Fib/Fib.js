776346fadd0a90e0b929ccd0e266bad288c3996a
Christine Gomez <christine.gomez1226@gmail.com>
2019-03-22 15:30:26 -0400

    memoize solution to fib

--- AlgoProbs/Fib/Fib.js
+++ AlgoProbs/Fib/Fib.js
@@ -27,5 +27,24 @@ function fib(n){
   return fib(n -1) + fib(n - 2)
 }
 
+//memoize function
+function memoize(fn){
+  const cache = {}
+  return function(...args){
+    if(cache[args]){
+      return cache[args]
+    }
+    const result = fn.apply(this, args)
+    cache[args] = result
+    return result
+  }
+}
+function slowfib(n){
+  if( n < 2){
+    return n
+  }
+  return fib(n -1) + fib(n - 2)
+}
+const fib = memoize(slowfib)
 
 module.exports = fib;
